bf2a85010fd23db7c6e68796a7db91f5c0e6b69c
DESKTOP-68QQH5S\Seongho <nsho77@naver.com>
2020-12-12 11:25:01 +0900

    Add transfroms to Renderer.

--- Hazel/src/Hazel/Renderer/Renderer.cpp
+++ Hazel/src/Hazel/Renderer/Renderer.cpp
@@ -14,10 +14,11 @@ namespace Hazel {
 	{
 	}
 
-	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray)
+	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform)
 	{
 		shader->Bind();
 		shader->UploadeUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
+		shader->UploadeUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
