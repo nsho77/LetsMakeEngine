6fc5c369ad43b97b900325554008e585fbd2c3e1
DESKTOP-68QQH5S\Seongho <nsho77@naver.com>
2021-01-18 21:46:40 +0900

    Organization
    - Many files move from src/Hazel to src/Hazel/Core.
    - Edit generate project batch file.

--- Hazel/src/Hazel/Renderer/OrthographicCameraController.cpp
+++ Hazel/src/Hazel/Renderer/OrthographicCameraController.cpp
@@ -1,8 +1,8 @@
 #include "hzpch.h"
 #include "OrthographicCameraController.h"
 
-#include "Hazel/Input.h"
-#include "Hazel/KeyCode.h"
+#include "Hazel/Core/Input.h"
+#include "Hazel/Core/KeyCode.h"
 
 namespace Hazel {
 
