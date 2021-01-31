95e73eeac9d666ff94800ecf870932bd943b6b24
DESKTOP-68QQH5S\Seongho <nsho77@naver.com>
2021-01-31 15:12:32 +0900

    Automated platform detection using predefined macro's

--- premake5.lua
+++ premake5.lua
@@ -82,7 +82,6 @@ project "Hazel"
 
 		defines
 		{
-			"HZ_PLATFORM_WINDOWS",
 			"HZ_BUILD_DLL",
 			"GLFW_INCLUDE_NONE"
 		}
@@ -133,11 +132,6 @@ project "Sandbox"
 
 	filter "system:windows"
 		systemversion "latest"
-
-		defines
-		{
-			"HZ_PLATFORM_WINDOWS"
-		}
 
 	filter "configurations:Debug"
 		defines "HZ_DEBUG"
